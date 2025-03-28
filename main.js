830bc4f245d2c90e008ca4daaa7f1c7e334fa6b8
atvars <avelavs@gmail.com>
2025-03-28 05:40:47 +0000

    updates

--- main.js
+++ main.js
@@ -1,10 +1,28 @@
+import { GoogleGenerativeAI } from "@google/generative-ai";
+
+const API_KEY = "AIzaSyD7bVkFIyWXalmhYV6kkEjwUYYsj5m9yKM";
+const genAI = new GoogleGenerativeAI(API_KEY);
+const model = genAI.getGenerativeModel({ 
+    model: "gemini-1.5-pro",
+    systemInstruction: businessInfo
+});
+
+
+
 function sendMessage() {
-  const userMessage = document.querySelector('.chat-window input').value;
+  const userMessage = document.querySelector(".chat-window input").value;
 
   if (userMessage.length){
-  alert(userMessage);
+    document.querySelector(".chat-window input").value = "";
+    document.querySelector(".chat-window .chat").insertAdjacentHTML("beforeend", `
+
+            <div class="user">
+                <p>${userMessage}</p>
+            </div>
+        `)
+
   }
 }
 
-const userMessage = document.querySelector('.chat-window .input-area button')
+document.querySelector('.chat-window .input-area button')
 .addEventListener('click', () => sendMessage());
