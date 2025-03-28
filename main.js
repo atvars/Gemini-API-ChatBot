17ab0e45401db52d11e7cfae1a18f469a57be59a
atvars <avelavs@gmail.com>
2025-03-28 04:26:49 +0000

    testing alert function with eventlisstener for module type js file

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
-
 function sendMessage() {
-    const userMessage = document.querySelector(".chat-window input").value
+  const userMessage = document.querySelector('.chat-window input').value;
 
-    alert (userMessage);
+  alert(userMessage);
 }
+
+const userMessage = document.querySelector('.chat-window .input-area button')
+.addEventListener('click', () => sendMessage());
