8b3abdf55d26add67c641be92c5e07ae43374f07
atvars <avelavs@gmail.com>
2025-04-04 00:38:11 +0000

    updated from start chat to sendMessageStream

--- main.js
+++ main.js
@@ -1,6 +1,5 @@
 import { GoogleGenerativeAI } from "@google/generative-ai";
 
-
 const businessInfo = `
 
 General Business Information:
@@ -89,58 +88,83 @@ const model = genAI.getGenerativeModel({
 
 // history array
 let messages = {
-  history: [],
+ history: [],
 }
 
 // sending message function
 async function sendMessage() {
-  const userMessage = document.querySelector(".chat-window input").value;
 
-  // make sure message is not empty add direct html and delete input field
-  if (userMessage.length){
+    // console.log(messages);
+    const userMessage = document.querySelector(".chat-window input").value;
+    
+    // make sure message is not empty add direct html and delete input field
+    if (userMessage.length) {
 
-    try {
-      document.querySelector(".chat-window input").value = "";
-      document.querySelector(".chat-window .chat").insertAdjacentHTML("beforeend", `
-              <div class="user">
-                  <p>${userMessage}</p>
-              </div>
-          `);
-  
-          // send message
-        const chat = model.startChat(messages);
-        let result = await chat.sendMessage(userMessage)
-  
-        document.querySelector(".chat-window .chat").insertAdjacentHTML("beforeend", `
-          <div class="model">
-              <p>${result.response.text()}</p>
-          </div>
-      `);
-  
-      // adding to history user input
-          messages.history.push({
-            role: "user",
-            parts:[{text: userMessage}],
-          });
-          
-      // // adding to history bot response
-          messages.history.push({
-            role: "model",
-            parts:[{text: result.response.text()}],
-          });
-      
-    } catch (error) {
-      document.querySelector(".chat-window .chat").insertAdjacentHTML("beforeend", `
-        <div class="error">
-            <p>Sorry, the message could not be sent!</p>
-        </div>
-    `);
-    }
+      // check for errors
+        try {
+            document.querySelector(".chat-window input").value = "";
+            document.querySelector(".chat-window .chat").insertAdjacentHTML("beforeend",`
+                <div class="user">
+                    <p>${userMessage}</p>
+                </div>
+            `);
 
+            document.querySelector(".chat-window .chat").insertAdjacentHTML("beforeend",`
+                <div class="loader"></div>
+            `);
+
+            const chat = model.startChat(messages);
+
+            let result = await chat.sendMessageStream(userMessage);
+            
+            document.querySelector(".chat-window .chat").insertAdjacentHTML("beforeend",`
+                <div class="model">
+                    <p></p>
+                </div>
+            `);
+            
+            let modelMessages = '';
+
+            for await (const chunk of result.stream) {
+              const chunkText = chunk.text();
+              modelMessages = document.querySelectorAll(".chat-window .chat div.model");
+              modelMessages[modelMessages.length - 1].querySelector("p").insertAdjacentHTML("beforeend",`
+                ${chunkText}
+            `);
+            }
+
+            messages.history.push({
+                role: "user",
+                parts: [{ text: userMessage }],
+            });
+
+            messages.history.push({
+                role: "model",
+                parts: [{ text: modelMessages[modelMessages.length - 1].querySelector("p").innerHTML }],
+            });
+
+        } catch (error) {
+            document.querySelector(".chat-window .chat").insertAdjacentHTML("beforeend",`
+                <div class="error">
+                    <p>The message could not be sent. Please try again.</p>
+                </div>
+            `);
+        }
+
+        document.querySelector(".chat-window .chat .loader").remove();
         
-
-  }
+    }
 }
 
-document.querySelector('.chat-window .input-area button')
-.addEventListener('click', () => sendMessage());
+document.querySelector(".chat-window .input-area button")
+.addEventListener("click", ()=>sendMessage());
+
+document.querySelector(".chat-button")
+.addEventListener("click", ()=>{
+    document.querySelector("body").classList.add("chat-open");
+});
+
+document.querySelector(".chat-window button.close")
+.addEventListener("click", ()=>{
+    document.querySelector("body").classList.remove("chat-open");
+});
